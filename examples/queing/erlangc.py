d73a0b6b677c105dd4c064ed85e1c9e59f4f003c
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-03-07 19:00:07 -0500

    Example of ErlangC usage

---
+++ examples/queing/erlangc.py
@@ -0,0 +1,30 @@
+"""
+Example taken from: https://www.callcentrehelper.com/erlang-c-formula-example-121281.htm
+
+Requirement: Find the number of agents required to manage call center transactions
+under the following parameters:
+
+Number of calls: 100
+In a period of minutes: 30
+Average Handling Time (seconds): 180 (3 minutes)
+Required Service Level: 80%
+Target Answer Time (Seconds): 20
+Maximum Occupancy: 85%
+Shrinkage: 30%
+"""
+
+from pyworkforce.queuing.erlang import ErlangC
+
+erlang = ErlangC(transactions=100, asa=20/60, aht=3, interval=30, max_occupancy=0.85, shrinkage=0.3)
+
+positions_requirements = erlang.required_positions(service_level=0.8)
+print("positions_requirements: ", positions_requirements)
+
+achieved_service_level = erlang.service_level(positions=positions_requirements['raw_positions'])
+print("achieved_service_level: ", achieved_service_level)
+
+waiting_probability = erlang.waiting_probability(positions=positions_requirements['raw_positions'])
+print("waiting_probability: ", waiting_probability)
+
+achieved_occupancy = erlang.achieved_occupancy(positions=positions_requirements['raw_positions'])
+print("achieved_occupancy: ", achieved_occupancy)
