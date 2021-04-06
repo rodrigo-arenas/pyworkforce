25f35fdece5a4402e83a99d7dad7506b2add2042
rodrigoarenas456 <31422766+rodrigoarenas456@users.noreply.github.com>
2021-04-06 13:07:27 -0500

    Upgraded example to use all cpu cores

--- README.md
+++ README.md
@@ -64,7 +64,7 @@ If you want to run different scenarios at the same time, you can use the MultiEr
 from pyworkforce.multi_search import MultiErlangC
 
 param_grid = {"transactions": [100], "aht": [3], "interval": [30], "asa": [20 / 60], "shrinkage": [0.3]}
-multi_erlang = MultiErlangC(param_grid=param_grid, n_jobs=4)
+multi_erlang = MultiErlangC(param_grid=param_grid, n_jobs=-1)
 
 required_positions_scenarios = {"service_level": [0.8, 0.85, 0.9], "max_occupancy": [0.8]}
 
--- examples/multi_search/erlang.py
+++ examples/multi_search/erlang.py
@@ -4,7 +4,7 @@ param_grid = {"transactions": [100, 200], "aht": [3], "interval": [30], "asa": [
 required_positions_scenarios = {"service_level": [0.85, 0.9], "max_occupancy": [0.8]}
 service_level_scenarios= {"positions": [10, 20, 30], "scale_positions": [True, False]}
 
-multi_erlang = MultiErlangC(param_grid=param_grid, n_jobs=4)
+multi_erlang = MultiErlangC(param_grid=param_grid, n_jobs=-1)
 
 required_positions_grid = multi_erlang.required_positions(arguments_grid=required_positions_scenarios)
 print(required_positions_grid)
@@ -13,7 +13,7 @@ service_level_grid = multi_erlang.service_level(arguments_grid=service_level_sce
 print(service_level_grid)
 
 param_grid = {"transactions": [100], "aht": [3], "interval": [30], "asa": [20 / 60], "shrinkage": [0.3]}
-multi_erlang = MultiErlangC(param_grid=param_grid, n_jobs=4)
+multi_erlang = MultiErlangC(param_grid=param_grid, n_jobs=-1)
 
 required_positions_scenarios = {"service_level": [0.7, 0.85, 0.9], "max_occupancy": [0.8]}
 
