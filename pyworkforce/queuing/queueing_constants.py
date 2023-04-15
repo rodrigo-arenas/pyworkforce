dc076a3575d7013f5d70f24aa0f6b5916563f26c
sfghuis <sfgroothuis@Hotmail.com>
2023-04-15 11:45:29 +0200

    expected constants

---
+++ pyworkforce/queuing/queueing_constants.py
@@ -0,0 +1,7 @@
+cTransactions_name = 'transactions'
+cShrinkage_name = 'shrinkage'
+cAHT_name = 'aht'
+cInterval_name = 'interval'
+cASA_name = 'asa'
+
+cErlangC_generic_variable_list = [cTransactions_name, cShrinkage_name, cAHT_name, cInterval_name, cASA_name]
